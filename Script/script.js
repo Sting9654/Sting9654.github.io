c9d6c366141a33a6e1d14cff2ff5682dba7a9552
Sting9654 <121906740+Sting9654@users.noreply.github.com>
2023-01-10 23:30:31 -0400

    Initial commit

---
+++ Script/script.js
@@ -0,0 +1,18 @@
+
+let menuVisibility = false;
+function showMenu(){
+    if(menuVisibility){
+        document.getElementById("nav").id="";
+        menuVisibility = false;
+    }
+    else{
+        document.getElementById("nav").id="nav-responsive";
+        menuVisibility = true;
+    }
+}
+
+function select(){
+    document.getElementById("nav");
+    menuVisible = false;
+} 
+
